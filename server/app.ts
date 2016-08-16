5e3b93405d5ee3d249aa2d896ef3469ba64ed9c7
clayton@xdevel.com.br <clayton@xdevel.com.br>
2016-08-16 02:01:52 -0300

    ajustado o projeto pra rodar ejs ao inves de hbs

--- server/app.ts
+++ server/app.ts
@@ -12,7 +12,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -20,7 +20,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../clients/web')));
 app.use('/', routes);
 
 // catch 404 and forward to error handler
